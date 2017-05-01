e8e37980d22c140a0ad72cf5bc5665f754355084
bgveenstra <b.g.veenstra@gmail.com>
2017-05-01 11:04:42 -0700

    add exercise 12-classes in-class code

--- es6-exercises/12-classes.js
+++ es6-exercises/12-classes.js
@@ -6,7 +6,25 @@
 // of the two original points, and whose y is the sum of their y properties.
 
 // Your code here
-console.log(new Point(1, 2).plus(new Point(2, 1)))
+class Point {
+  // constructor is the function that gets run when we create a new point
+  constructor(x,y){
+    this.x = x;
+    this.y = y;
+  }
+  // take in another point and return the combination of points
+  // newPoint.x = thisPoint.x + otherPoint.x,  newPoint.y = thisPoint.y + otherPoint.y
+  plus(otherPoint){  // point just has structure {x: some number, y: some other number}
+    // ...
+    let newX = this.x + otherPoint.x   // addition of x from each point
+    let newY = this.y + otherPoint.y
+    return new Point(newX, newY)
+  }
+}
+// console.log(new Point(1,2).x)
+let pointA = new Point(1, 2)
+let pointB = new Point(2, 1)
+console.log(pointA.plus(pointB))
 // → Point{x: 3, y: 3}
 
 
